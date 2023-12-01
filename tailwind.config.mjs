c42068887dc978f9c70a4bd6103638c3964b9f7d
Alvisex <alvise.ld@gmail.com>
2023-12-01 00:38:58 -0600

    stuff

---
+++ tailwind.config.mjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+		container: {
+			center: true,
+			padding: {
+				DEFAULT: '1rem',
+				sm: '2rem',
+				lg: '5rem',
+				xl: '7rem',
+				'2xl': '8rem',
+			},
+			screens: {
+				sm: '600px',
+				md: '728px',
+				lg: '984px',
+				xl: '1200px',
+				'2xl': '1496px',
+			},
+		}
+	},
+	plugins: [],
+}
