92febe6cb164480e083b3624125e7879ea816b9f
Alvisex <alvise.ld@gmail.com>
2024-03-05 13:26:14 -0600

    add 3d model

---
+++ src/components/TheExperience4.vue
@@ -0,0 +1,145 @@
+<script setup lang="ts">
+import { TresCanvas, useSeek } from '@tresjs/core'
+import { BasicShadowMap, SRGBColorSpace, NoToneMapping, Vector3 } from 'three'
+import { OrbitControls, Stars, GLTFModel, CameraControls } from '@tresjs/cientos'
+import { ref, shallowRef, watch, onMounted, watchEffect } from 'vue'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
+import { useControls, TresLeches } from '@tresjs/leches'
+import '@tresjs/leches/dist/style.css'
+
+/* const color = ref('#00f')
+const intensity = ref(1) */
+
+const { intensity, color, positionn } = useControls({
+  intensity: {
+    value: 5.1,
+    min: 0,
+    max: 10,
+    step: 0.01
+  },
+  color: '#df6bff',
+  positionn: new Vector3(-2, 1, -4)
+})
+const { intensity2, color2, positionn2 } = useControls({
+  intensity2: {
+    value: 5.5,
+    min: 0,
+    max: 10,
+    step: 0.01
+  },
+  color2: '#52b7a3',
+  positionn2: new Vector3(4, -6, 3)
+})
+
+watchEffect(() => {
+  console.log(intensity.value.value)
+  console.log(color.value.value)
+  console.log('position', positionn.value)
+})
+
+gsap.registerPlugin(ScrollTrigger)
+
+const gl = {
+  shadows: true,
+  alpha: true,
+  shadowMapType: BasicShadowMap,
+  outputColorSpace: SRGBColorSpace,
+  toneMapping: NoToneMapping
+}
+
+const cameras = [
+  { x: 5, y: 6, z: 6 },
+  { x: -5, y: 3, z: 3 },
+  { x: 4, y: 7, z: 0 },
+  { x: 3, y: 5, z: -5 },
+  { x: 6, y: 3, z: -1 }
+]
+
+const cameraRef = ref()
+let sections: Element[]
+function aa() {
+  console.log('cameraRef', cameraRef.value)
+}
+
+const modelRef = shallowRef<THREE.Object3D>()
+watch(modelRef, (model) => {
+  loadAnimation()
+
+  console.log('modelRef', modelRef.value.value)
+  const { seekByName } = useSeek()
+  const nimbus001 = seekByName(model.value, 'nimbus001')
+  const nimbus002 = seekByName(model.value, 'nimbus002')
+  const nimbus003 = seekByName(model.value, 'nimbus003')
+  gsap.to(nimbus002.rotation, { z: Math.PI * 2, duration: 28, ease: 'none', repeat: -1 })
+  gsap.to(nimbus001.rotation, { z: -Math.PI * 2, duration: 40, ease: 'none', repeat: -1 })
+  gsap.to(nimbus003.rotation, { z: -Math.PI * 2, duration: 60, ease: 'none', repeat: -1 })
+  gsap.to(nimbus002.rotation, { y: 0.2, duration: 5, ease: 'none', repeat: -1, yoyo: true })
+  gsap.to(nimbus001.rotation, { y: -0.2, duration: 5, ease: 'none', repeat: -1, yoyo: true })
+})
+
+const loadAnimation = () => {
+  console.log('loadanimation', sections, modelRef.value.value)
+
+  sections.forEach((element, index) => {
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: element,
+          markers: false,
+          start: 'top center',
+          end: 'bottom 60%',
+          toggleActions: 'play complete  reverse complete '
+        },
+        defaults: {
+          duration: 2,
+          ease: 'power3'
+        }
+      })
+      .to(cameraRef.value.position, {
+        ...cameras[index],
+        onUpdate: () => {
+          cameraRef.value.lookAt(0, 4, 0)
+        }
+      })
+  })
+}
+
+onMounted(() => {
+  sections = gsap.utils.toArray('section')
+  console.log(`sections`, sections)
+})
+</script>
+
+<template>
+  <div class="w-full h-screen">
+    <!--  <TresLeches /> -->
+    <TresCanvas v-bind="gl" id="mycanvas">
+      <TresPerspectiveCamera ref="cameraRef" :position="[1, 5, 6]" :look-at="[0, 4, 0]" />
+      <!--  <OrbitControls /> -->
+      <Stars />
+      <Suspense>
+        <GLTFModel path="/models/rhetorician/scene.gltf" draco ref="modelRef" />
+      </Suspense>
+
+      <!-- <TresGridHelper :position="[0, 0, 0]" /> -->
+      <TresDirectionalLight :intensity="intensity.value" :color="color.value" :position="positionn.value.toArray()" />
+      <TresDirectionalLight :intensity="intensity2.value" :color="color2.value" :position="positionn2.value.toArray()" />
+    </TresCanvas>
+    <!-- <button @click="aa">log</button> -->
+    <!--  <pre>
+      {{ intensity }}
+    </pre> -->
+    <!-- <div class="as"></div> -->
+  </div>
+</template>
+
+<style>
+canvas#mycanvas {
+  position: fixed !important;
+  z-index: -1;
+}
+.tl-absolute {
+  top: 100px !important;
+}
+</style>
