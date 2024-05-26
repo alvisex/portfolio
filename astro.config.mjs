7fba3bd3fc9b381e85bbf8a30d8b9ed9e8a9fbd8
alvisex <alvise.ld@gmail.com>
2024-05-25 20:00:01 -0600

    dynamic experience cards

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
-
 import tailwind from "@astrojs/tailwind";
+import yaml from '@rollup/plugin-yaml';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind()]
+  integrations: [vue(), tailwind()],
+  vite: {
+    plugins: [yaml()]
+  }
 });
---
+++ src/components/Items.vue
@@ -0,0 +1,15 @@
+<script setup>
+const props = defineProps(['items'])
+const items = props.items
+</script>
+
+<template>
+  <ul class="list-disc list-inside">
+    <li v-for="item in items" v-html="item"></li>
+  </ul>
+</template>
+<style>
+strong {
+  @apply text-accent;
+}
+</style>
