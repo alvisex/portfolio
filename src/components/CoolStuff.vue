73d319f2b2b82df968406ea236d448c53c2a2a24
alvisex <alvise.ld@gmail.com>
2026-03-26 18:38:11 -0600

    fix: fix tres models

--- src/components/CoolStuff.vue
+++ src/components/CoolStuff.vue
@@ -1,42 +1,15 @@
 <script setup lang="ts">
 import { TresCanvas } from '@tresjs/core'
 import { BasicShadowMap, SRGBColorSpace, NoToneMapping } from 'three'
-import { GLTFModel } from '@tresjs/cientos'
+import { useGLTF } from '@tresjs/cientos'
 import { ref, shallowRef, watch, onMounted } from 'vue'
+
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
-/* import { useControls, TresLeches } from '@tresjs/leches'
-import '@tresjs/leches/dist/style.css' */
 gsap.registerPlugin(ScrollTrigger)
 
-/* const { intensity, color, positionn } = useControls({
-  intensity: {
-    value: 5.1,
-    min: 0,
-    max: 10,
-    step: 0.01,
-  },
-  color: '#fff36b',
-  positionn: new Vector3(3, 1, -4),
-})
-const { intensity2, color2, positionn2 } = useControls({
-  intensity2: {
-    value: 5.5,
-    min: 0,
-    max: 10,
-    step: 0.01,
-  },
-  color2: '#ff42a7',
-  positionn2: new Vector3(-3, 2, 3),
-}) */
+import type { TresObject } from 'tresjs'
 
-const gl = {
-  shadows: false,
-  alpha: true,
-  shadowMapType: BasicShadowMap,
-  outputColorSpace: SRGBColorSpace,
-  toneMapping: NoToneMapping,
-}
 
 const cameras = [
   { x: -5, y: 3, z: 3.5 },
@@ -57,22 +30,26 @@ const lookAtAux = { x: 0, y: 4, z: 0 }
 
 const cameraRef = ref()
 let sections: Element[]
-function aa() {
-  console.log('cameraRef', cameraRef.value)
-}
 
-const modelRef = shallowRef<THREE.Object3D>()
-watch(modelRef, (model) => {
+
+const path = "/models/alterado.glb"
+
+const { state, nodes, isLoading, progress } = useGLTF(path, { draco: true })
+
+
+watch(isLoading, () => {
   setTimeout(() => {
-    loadAnimation(model)
-    haloAnimation(model)
+    haloAnimation(nodes)
+    loadAnimation(nodes)
   }, 200)
+
 })
 
-const haloAnimation = (model) => {
-  const nimbus001 = model.value.getObjectByName('nimbus001')
-  const nimbus002 = model.value.getObjectByName('nimbus002')
-  const nimbus003 = model.value.getObjectByName('nimbus003')
+const haloAnimation = (nodes: any) => {
+  const nimbus001 = nodes.value?.nimbus001
+  const nimbus002 = nodes.value?.nimbus002
+  const nimbus003 = nodes.value?.nimbus003
+
   gsap.to(nimbus002.rotation, { z: Math.PI * 2, duration: 28, ease: 'none', repeat: -1 })
   gsap.to(nimbus001.rotation, { z: -Math.PI * 2, duration: 40, ease: 'none', repeat: -1 })
   gsap.to(nimbus003.rotation, { z: -Math.PI * 2, duration: 60, ease: 'none', repeat: -1 })
@@ -80,9 +57,10 @@ const haloAnimation = (model) => {
   gsap.to(nimbus001.rotation, { y: -0.2, duration: 5, ease: 'none', repeat: -1, yoyo: true })
 }
 
-const loadAnimation = (model) => {
-  const lentes = model.value.getObjectByName('lentes')
-  const busto = model.value.getObjectByName('busto')
+const loadAnimation = (nodes: any) => {
+  // console.log('load animation', nodes)
+  const lentes = nodes.value?.lentes
+  const busto = nodes.value?.busto
 
   /* Intro animation */
   gsap
@@ -92,7 +70,7 @@ const loadAnimation = (model) => {
         trigger: '.intro',
         start: 'top bottom',
         end: 'bottom 80%',
-        //markers: true,
+        // markers: true,
         scrub: true,
       },
     })
@@ -102,7 +80,7 @@ const loadAnimation = (model) => {
     .from(busto?.position, { x: -4, y: -5.5, z: 0 }, 0)
 
   /* Animation per section */
-  sections.forEach((element: Element, index) => {
+  sections.forEach((element: any, index) => {
     const { endMark } = element.dataset
     gsap
       .timeline({
@@ -130,46 +108,29 @@ const loadAnimation = (model) => {
       )
   })
 
-  setTimeout(() => {
-    gsap.set('canvas', { opacity: 1 })
-  }, 100)
+  gsap.set('canvas#mycanvas', { opacity: 1, })
+
 }
 
-const screenWidth = ref(0)
 onMounted(() => {
   sections = gsap.utils.toArray('[data-model]')
-  screenWidth.value = window.innerWidth
-  console.log('screenWidth', screenWidth.value)
 })
 </script>
 
 <template>
-  <!--  <TresLeches class="fixed" /> -->
-  <!--  <div class="the experience sticky top-0 h-screen"> -->
-  <div class="h-screen intro" v-if="screenWidth > 768">
-    <TresCanvas v-bind="gl" id="mycanvas" window-size>
-      <TresPerspectiveCamera ref="cameraRef" :position="[1, 5, 6]" :look-at="[0, 4, 0]" />
-      <Suspense>
-        <GLTFModel path="/models/alterado.glb" draco ref="modelRef" />
-      </Suspense>
+  <TresPerspectiveCamera ref="cameraRef" :position="[1, 5, 6]" :look-at="[0, 4, 0]" />
 
-      <TresAmbientLight :intensity="0" :color="'#fff'" />
-
-      <TresDirectionalLight :intensity="5.1" :color="'#fff36b'" :position="[3, 1, -4]" />
-      <TresDirectionalLight :intensity="5.2" :color="'#ff42a7'" :position="[-3, 2, 3]" />
-    </TresCanvas>
-  </div>
-
-  <div v-else class="h-32"></div>
-  <!--  </div> -->
+  <primitive v-if="state" :object="state?.scene"  />
+  
+  
+  <TresAmbientLight :intensity="0" :color="'#fff'" />
+ <!--  <TresGridHelper :args="[10, 10]" /> -->
+  <TresDirectionalLight :intensity="5.1" :color="'#fff36b'" :position="[3, 1, -4]" />
+  <TresDirectionalLight :intensity="5.2" :color="'#ff42a7'" :position="[-3, 2, 3]" />  
 </template>
 
 <style lang="scss">
-canvas#mycanvas {
-  z-index: 0;
-  pointer-events: none !important;
-  opacity: 0;
-}
+
 .tl-absolute {
   top: 100px !important;
   position: fixed;
